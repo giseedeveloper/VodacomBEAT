1454f86bd49a5c6e72ce28fca3b40eba5761d5b3
Charden RJUK <rjkilonzo@gmail.com>
2023-10-16 20:05:20 +0300

    Vodacom Tunes Portal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                <p style="color: aqua">NBC-VPL api</p>
+                <p style="color: aqua">Vodacom Tune API</p>
             </div>
         </div>
     </body>
